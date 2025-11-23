d5a26a10a9e9a616a72bc48a8ac9e10c5a153c72
Zanaira Kanwal <zanairakanwal6870@gmail.com>
2025-11-23 22:17:21 +0500

    Rename class LabTask4 to lab4

--- lab4.java
+++ lab4.java
@@ -1,6 +1,7 @@
-public class LabTask4 {
+public class lab4 {
     public static void main(String[] args) {
         char ch = (char) ('A' + Math.random() * 26);
         System.out.println("Random uppercase letter: " + ch);
     }
+
 }
